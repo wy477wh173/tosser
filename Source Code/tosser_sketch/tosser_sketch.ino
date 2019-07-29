29cf33bc01aa9872559d532b5bab0a4a1bbcd147
Rhindle <wyattwhite@cakeneq.com>
2019-07-29 09:35:22 -0800

    First Commit

---
+++ Source Code/tosser_sketch/tosser_sketch.ino
@@ -0,0 +1,18 @@
+//Tosser
+//Simple Artilery Clone for Arduboy
+//Credit; RMW Games: Robert White, Michael White, Wyatt White
+
+//importing arduboy libraries
+#include <Arduboy2.h>
+Arduboy2 ardb;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
