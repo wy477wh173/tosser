795d9b9856ebd2cf48ad0abf22268226455ca764
Bob White <bobwhiteconst@gmail.com>
2019-07-30 20:32:15 -0800

    Wyatt used = instead of ==

--- Source Code/tosser_sketch/tosser_sketch.ino
+++ Source Code/tosser_sketch/tosser_sketch.ino
@@ -11,7 +11,7 @@ byte drawx;
 byte drawy;
 char text[]= "RMW Games";
 
-byte draw_text;
+byte draw_text = 0;
 
 
 //comument
@@ -49,24 +49,27 @@ void loop() {
     }
 
   //check if A pressed
-  draw_text = 0;
+  //draw_text = 0;
   if(ardb.pressed(A_BUTTON))
     {
-    draw_text = 1;
+      if(draw_text == 1)
+        {
+        draw_text = 0;
+        }else{draw_text = 1;}
     }
 
-//setting graphics
+  //setting graphics
   //clear last frame gfx
   ardb.clear();
 
   //check if text_draw bitis flipped
-  if(draw_text = 0)
+  if(draw_text == 0)
   {
     //draw cursor if button not pressed this frame
     ardb.drawPixel(drawx,drawy);
   }
 
-  if(draw_text = 1)
+  if(draw_text == 1)
     {
       //draw text if A pressed this frame
       ardb.setCursor(drawx,drawy);
