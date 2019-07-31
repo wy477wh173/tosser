79c114feb63cf73877aa3eae477db956aba9f8ff
Rhindle <wyattwhite@cakeneq.com>
2019-07-31 10:12:45 -0800

    Actual Start I moved our RMW Games test into a separate sketch added a really simple outline for a menu Menu uses the same value to Draw cursor rectangle, menu items, and confirm state It might be too fat rn, but idk.

---
+++ Source Code/test_sketch/test_sketch.ino
@@ -0,0 +1,76 @@
+//Tosser
+//Simple Artilery Clone for Arduboy
+//Credit; RMW Games: Robert White, Michael White, Wyatt White
+
+//importing arduboy libraries
+#include <Arduboy2.h>
+Arduboy2 ardb;
+
+//variable init
+byte drawx;
+byte drawy;
+char text[]= "RMW Games";
+
+byte draw_text = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  ardb.begin();
+  ardb.setFrameRate(30);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //move cursor
+  //up
+  if(ardb.pressed(UP_BUTTON))
+    {
+      drawy --;
+    }
+  //down
+  if(ardb.pressed(DOWN_BUTTON))
+    {
+      drawy ++;
+    }
+
+  //left
+  if(ardb.pressed(LEFT_BUTTON))
+    {
+      drawx --;
+    }
+  //right
+  if(ardb.pressed(RIGHT_BUTTON))
+    {
+      drawx ++;
+    }
+
+  //check if A pressed
+  draw_text = 0;
+  if(ardb.pressed(A_BUTTON))
+    {
+      draw_text = 1;
+    }
+
+  //setting graphics
+  //clear last frame gfx
+  ardb.clear();
+
+  //check if text_draw bitis flipped
+  if(draw_text == 0)
+  {
+    //draw cursor if button not pressed this frame
+    ardb.drawRoundRect(drawx,drawy,16,8,4);//makde this a round rect for fun.
+  }
+
+  if(draw_text == 1)
+    {
+      //draw text if A pressed this frame
+      ardb.setCursor(drawx,drawy);
+      ardb.write(text);
+    }
+// draw screen
+ardb.display();
+}
--- Source Code/tosser_sketch/tosser_sketch.ino
+++ Source Code/tosser_sketch/tosser_sketch.ino
@@ -5,16 +5,71 @@
 //importing arduboy libraries
 #include <Arduboy2.h>
 Arduboy2 ardb;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////Definitions
+//variable init // these were used in the initial test, have since been removed
+//byte drawx;
+//byte drawy;
+//char intro[]= "RMW Games";
 
-//variable init
-byte drawx;
-byte drawy;
-char text[]= "RMW Games";
+//menu definitions
+const char play[] = "Play";
+const char help[] = "Help";
+const char credit[] = "Credit";
 
-byte draw_text = 0;
+//didn't actually need these, so commented out, just used math instead
+//const byte playy = 16;
+//const byte helpy = 32;
+//const byte credity = 48;
 
+//define game state (0 = Menu, 1 = game, 2 = help, 3 = credit) 
+byte game_state = 0;
+byte menu_state = 1;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////Definitions End
 
-//comument
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////Game States
+void draw_menu()
+  {//this should draw a really simple menu
+  //draw cursor @ 16 * menu_state
+  //should draw a rounded rectangle under the current selection
+   ardb.drawRoundRect(2,16*menu_state,32,16,5);
+    
+   //play item // menu state 1
+    ardb.setCursor(3,16 * 1);
+    ardb.write(play);
+   
+   //help item // menu state 2
+    ardb.setCursor(3,16 * 2);
+    ardb.write(help);
+
+   //credit item // menu state 3
+    ardb.setCursor(3,16 * 3);
+    ardb.write(credit);
+   
+   //move cursor
+    if(ardb.justPressed(UP_BUTTON) && menu_state > 1)
+      {
+      menu_state += -1;
+      }
+
+      if(ardb.justPressed(DOWN_BUTTON) && menu_state < 2)
+      {
+      menu_state += 1;
+      }
+
+    if(ardb.justPressed(A_BUTTON))
+     {
+      //Sets game state = menu state // (0 = Menu, 1 = game, 2 = help, 3 = credit) 
+      game_state = menu_state;
+     }
+  }
+
+void draw_credit()
+  {//this should just draw our names
+    
+  }
+/////////////////////////////////////////////////////////////////////////////////////////////////Game states end
+
+/////////////////////////////////////////////////////////////////////////////////////////////////Master loops
 void setup() {
   // put your setup code here, to run once:
   ardb.begin();
@@ -22,59 +77,21 @@ void setup() {
   
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-  //move cursor
-  //up
-  if(ardb.pressed(UP_BUTTON))
-    {
-      drawy --;
-    }
-  //down
-  if(ardb.pressed(DOWN_BUTTON))
-    {
-      drawy ++;
-    }
-
-  //left
-  if(ardb.pressed(LEFT_BUTTON))
-    {
-      drawx --;
-    }
-  //right
-  if(ardb.pressed(RIGHT_BUTTON))
-    {
-      drawx ++;
-    }
-
-  //check if A pressed
-  //draw_text = 0;
-  if(ardb.pressed(A_BUTTON))
-    {
-      if(draw_text == 1)
-        {
-        draw_text = 0;
-        }else{draw_text = 1;}
-    }
-
-  //setting graphics
-  //clear last frame gfx
-  ardb.clear();
-
-  //check if text_draw bitis flipped
-  if(draw_text == 0)
+void loop() 
   {
-    //draw cursor if button not pressed this frame
-    ardb.drawPixel(drawx,drawy);
-  }
+  // put your main code here, to run repeatedly:
+  ardb.clear();
+  if(game_state == 0)//menu
+    {draw_menu();}
+  //if(game_state == 1)//menu
+  //  {draw_game();}
+  //if(game_state == 2)//help
+  //  {draw_menu();}
+  if(game_state == 3)//credit
+    {draw_credit();}
 
-  if(draw_text == 1)
-    {
-      //draw text if A pressed this frame
-      ardb.setCursor(drawx,drawy);
-      ardb.write(text);
-    }
-// draw screen
-ardb.display();
-}
+    
+  // draw screen
+  ardb.display();
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////////Master loops end
